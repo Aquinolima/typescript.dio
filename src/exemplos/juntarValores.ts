51d5c015ba851b34d2361cc81693f34fb64c072a
Thiago Aquino <aquinolima.thiago@gmail.com>
2022-06-20 22:33:47 -0300

    intro typescript - functions.ts

---
+++ src/exemplos/juntarValores.ts
@@ -0,0 +1,15 @@
+/* type input = number | string;
+
+function juntarValores(input1: input, input2: input) {
+    let resultado: input;
+    if (typeof input1 === 'string' || typeof input2 === 'string') {
+        resultado = input1.toString() + input2.toString();
+    } else {
+        resultado = input1 + input2
+    }
+    return resultado;
+}
+
+console.log(juntarValores('olá ', 'tudo bem?'));
+console.log(juntarValores(1, 5));
+console.log(juntarValores("1", 5)); */
